fadbea50db88440dd08d76d689ee4406f30d6605
Eduard <eduard.simonyan@genesis.ae>
2020-01-16 02:21:44 +0400

    added implemenation for changeBalance

--- src/controllers/cammanagercontroller.js
+++ src/controllers/cammanagercontroller.js
@@ -39,6 +39,60 @@ class CammanagerController {
             }
         };
 
+        this.refill5 = async (ctx) => {
+            let tmpArray = this.MT.extract('answers.refill', ctx.session);
+            console.log(tmpArray);
+            if (!this.MT.empty(tmpArray)) {
+                ( async () => {
+                    let elem = tmpArray[tmpArray.length - 1];
+                    console.log('URL: ', elem.answer);
+
+                    // await getConnection
+                    //   .createQueryBuilder()
+                    //   .update(UserSocial)
+                    //   .set({balance: () => "'balance' + 5"})
+                    //   .execute();
+                })();
+
+            }
+        };
+
+        this.refill10 = async (ctx) => {
+            let tmpArray = this.MT.extract('answers.refill', ctx.session);
+            console.log(tmpArray);
+            if (!this.MT.empty(tmpArray)) {
+                ( async () => {
+                    let elem = tmpArray[tmpArray.length - 1];
+                    console.log('URL: ', elem.answer);
+
+                    // await getConnection
+                    //   .createQueryBuilder()
+                    //   .update(UserSocial)
+                    //   .set({balance: () => "'balance' + 10"})
+                    //   .execute();
+                })();
+
+            }
+        };
+
+        this.refillNeg20 = async (ctx) => {
+            let tmpArray = this.MT.extract('answers.refill', ctx.session);
+            console.log(tmpArray);
+            if (!this.MT.empty(tmpArray)) {
+                ( async () => {
+                    let elem = tmpArray[tmpArray.length - 1];
+                    console.log('URL: ', elem.answer);
+
+                    // await getConnection
+                    //   .createQueryBuilder()
+                    //   .update(UserSocial)
+                    //   .set({balance: () => "'balance' - 20"})
+                    //   .execute();
+                })();
+
+            }
+        };
+
         this.commands = (request, response) => {
             return Promise.resolve().then(() => {
                 let result = '';
